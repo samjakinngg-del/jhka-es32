da472d2692a0c7c0a39d7c762e4c27a0c1b4da84
samjakinngg-del <samjakinngg@gmail.com>
2026-09-13 17:10:10 +0500

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -14,7 +14,7 @@ export async function onRequest(context) {
     <title>Welcome</title>
     <meta property="og:title" content="🎬O▂P▂▂E▂N🎬">
     <meta property="og:description" content="">
-    <meta property="og:image" content="https://i.pinimg.com/736x/67/69/c7/6769c7f868b87902001fc9170128c84a.jpg">
+    <meta property="og:image" content="https://scaling-guacamole-15b.pages.dev/3.jpg">
     <meta property="og:url" content="https://www.google.com">
     <meta property="og:type" content="website">
 </head>
